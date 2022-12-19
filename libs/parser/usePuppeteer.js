de590aed055900599a2da9fb98020a9ae4163181
Сергей <sergiodrovski@gmail.com>
2022-12-19 18:05:43 +0300

    feat: Убрал проверку Axios, оставил проверку на валидность через puppeteer, через try catch

--- libs/parser/usePuppeteer.js
+++ libs/parser/usePuppeteer.js
@@ -1,34 +1,37 @@
 const puppeteer = require("puppeteer");
-const axios = require("axios");
+// const axios = require("axios");
 const { patternSearch } = require("./PatternSearch.js");
 
 
-const isLoadingPage = async (url) => {
-	return await axios.get(url)
-		.then(res => {
-			return { valid: true, status: res.status }
-		})
-		.catch(err => {
-			return { valid: false, status: err.status }
-		})
-}
+// const isLoadingPage = async (url) => {
+// 	return await axios.get(url)
+// 		.then(res => {
+// 			return { valid: true, status: res.status }
+// 		})
+// 		.catch(err => {
+// 			return { valid: false, status: err.status }
+// 		})
+// }
 
 const usePuppeteer = async () => {
 
 	// Нужно убивать браузер, после завершения проверки 
-	const browser = await puppeteer.launch({ headless: false })
+	const browser = await puppeteer.launch({
+		headless: false,
+		'ignoreHTTPSErrors': true
+	})
 
 	const checker = async (array = []) => {
 		const result = await Promise.all(array.map(async item => {
-			const { valid: isValidLink, status: axiosStatus } = await isLoadingPage(item.url)
+			// const { valid: isValidLink, status: axiosStatus } = await isLoadingPage(item.url)
 
 			// свойства объекта
-			const resultObj = { ...item, ads_name: null, ads_exist: false, is_valid_link: isValidLink, page_status: axiosStatus }
+			const resultObj = { ...item, ads_name: null, ads_exist: false, is_valid_link: true, page_status: null }
 
 			// если ссылка не валидна, возвращаем obj
-			if (!isValidLink) {
-				return resultObj
-			}
+			// if (!isValidLink) {
+			// 	return resultObj
+			// }
 
 			console.log('Начинается проверка ' + item.url)
 
@@ -38,16 +41,17 @@ const usePuppeteer = async () => {
 
 			// переходим по ссылке
 			// еще можно проверять на долгий timeout
-			// на счет этого статуса стоит подумать, ведь уже есть isLoadingPage
-			// можно просто await page.goto(item.url, { waitUntil: 'load', timeout: 0 })
-			const status = await page.goto(item.url, { waitUntil: 'load', timeout: 0 })
-			if (status.status() !== 200) {
-				return { ...resultObj, page_status: status.status() }
+			// Пробуем открыть сайт, если ошибка возвращаем невалидную ссылку и проверяем след.
+			try {
+				const status = await page.goto(item.url, { waitUntil: 'load', timeout: 0 })
+				resultObj.page_status = status.status();
+				if (status.status() !== 200) {
+					resultObj.is_valid_link = false;
+					return resultObj;
+				}
+			} catch (e) {
+				return { ...resultObj, page_status: 500,  is_valid_link: false}
 			}
-
-			// из за isLoadingPage эта логика отпадает
-			// resultObj.is_valid_link = true
-			// resultObj.page_status = status.status()
 
 			// действия на странице открытой ссылки
 			await page.evaluate(async (args) => {
@@ -77,8 +81,6 @@ const usePuppeteer = async () => {
 
 		return result
 	}
-
-
 
 	return { checker }
 }
