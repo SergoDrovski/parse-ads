ae0a581664510bd0534155f9ff7001f7867335db
Сергей <sergiodrovski@gmail.com>
2022-12-19 18:00:41 +0300

    feat: Поменял общий поиск по регуляркам. Добавил регулярки по нескольким партнёрам

--- libs/parser/PatternSearch.js
+++ libs/parser/PatternSearch.js
@@ -1,50 +1,73 @@
-
-// { ads_exist: bool, ads_name: String }
-
 exports.patternSearch = function patternSearch() {
+
+    // Поочерёдный Запуск проверки на каждого партнёра
     function syncAll(patterns) {
-        const node = document.body;
+        const node = document.body.outerHTML;
         let res;
 
         for (let i = 0; i < patterns.length; i++) {
-            res = patterns[i](node);
-            if(res.ads_exist) break;
+            res = search(patterns[i], node);
+            if (res.ads_exist) break;
         }
         return res;
     }
 
-    function patternLucky(element) {
-        const patternsRegex = {
+    // Общая функция проверки, получаем регулярку и элемен по которому ищем
+    function search(patterns, element) {
+
+        let result = {
+            ads_exist: false,
+            ads_name: null,
+            pattern_find: null
+        };
+
+        // Поск делаю по outerHTML всего body(проходимся по строке регуляркой)
+        for (let type in patterns.regex) {
+            let r = element.match(patterns.regex[type]);
+            if (r) {
+                result.pattern_find = type + ": " + patterns.regex[type];
+                result.ads_exist = true;
+                result.ads_name = patterns.ads_name;
+                break
+            }
+        }
+
+        return result;
+    }
+
+
+    // Правила проверки партнёров (регулярки)
+    const Lucky = {
+        ads_name: 'Lucky',
+        regex: {
             class: /data-la-show-block-id-[A-Za-z0-9-="]*/gm,
             link: /https:\/\/.*click\?media=/gm,
             attr: /rel="sponsored/gm
-        };
-        let result = {
-            ads_exist: false,
-            ads_name: null
-        };
-
-        function search(el) {
-            const adsElement = [...el.querySelectorAll('div')]
-                .reverse()
-                .find(el => {
-                    for(let type in patternsRegex){
-                        if(el.outerHTML.match(patternsRegex[type])) return true
-                    }
-                    return false
-                })
-            if(adsElement) {
-                result.ads_exist = true;
-                result.ads_name = 'Lucky';
-            }
-            return result;
         }
+    };
 
-        return search(element);
-    }
+    const Adwail = {
+        ads_name: 'Adwail',
+        regex: {
+            link: /adwile.com/gm,
+            class: /smi24__informer/gm,
+            attr: /data-smi-blockid/gm
+        }
+    };
 
+    const Directadvert = {
+        ads_name: 'Directadvert',
+        regex: {
+            link: /code.directadvert.ru/gm,
+            class: /directadvert-block/gm,
+        }
+    };
+
+
+    // Запуск проверки
     return syncAll([
-        patternLucky,
-
+        Lucky,
+        Adwail,
+        Directadvert
     ])
 }
