203e7821c1d45d0a87290052a49b03a5eea2f7a8
Сергей <sergiodrovski@gmail.com>
2022-12-27 22:00:17 +0300

    feat: перенос в докер, пререключение базы на удалённый mongodb+sr

--- frontend/dist/assets/Task-7d7f9813.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./index-1f49e2bd.js";const o={};function r(s,t){return c(),n("div",null," Task.vue ")}const _=e(o,[["render",r]]);export{_ as default};
--- frontend/dist/assets/TaskStop-9f14e30f.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-1f49e2bd.js";const t={};function n(r,s){return c(),o("div",null," TaskStop.vue ")}const _=e(t,[["render",n]]);export{_ as default};
